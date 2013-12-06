5d77ecd51ee8e039dbe0d56c0ad9c678c3e03c59
Eymen Gunay <eymen@egunay.com>
2013-12-06 03:21:55 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# First make sure capifony is installed
+if ! type capifony &> /dev/null ; then
+    info "capifony was not found, installing"
+    gem install capifony
+else
+    info "capifony is available"
+    debug "capifony version: $(capifony --version)"
+fi
+
+capifony_command="cap"
+
+# Parse some variable arguments
+if [ -n "$WERCKER_CAP_STAGE" ] ; then
+    capifony_command="$capifony_command $WERCKER_CAP_STAGE"
+fi
+
+if [ -n "$WERCKER_CAP_TASKS" ] ; then
+    capifony_command="$capifony_command $WERCKER_CAP_TASKS"
+else
+    capifony_command="$capifony_command deploy"
+fi
+
+debug "$capifony_command"
+$capifony_command
+
+if [[ $? -ne 0 ]]; then
+    warn "$result"
+    fail "capifony command failed"
+else
+    success "finished $capifony_command"
+fi
